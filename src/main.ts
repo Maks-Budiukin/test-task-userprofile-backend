f9e68a03b605d8c1b00d47db5ab32b44c3774368
Maks-Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2023-09-14 18:01:15 +0300

    validation, hot reload, cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+declare const module: any;
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.enableCors();
+  await app.listen(4000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
